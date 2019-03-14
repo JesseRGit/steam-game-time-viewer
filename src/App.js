2e0a9324a4672674fbd7c6faf070b1dea0202082
JR <jesse.ronkko@eng.tamk.fi>
2019-03-14 19:58:14 +0200

    Saving state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+
+const API_KEY = process.env.REACT_APP_STEAM_API_KEY;
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    };
+  }
+
+  componentDidMount() {
+    var url = `https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${API_KEY}&steamid=76561197980934566&format=json`;
+    console.log(url);
+    fetch(url).then(
+      response => response.json()
+    ).then(
+      json => {
+        console.log(json);
+        this.setState({data: json});
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
