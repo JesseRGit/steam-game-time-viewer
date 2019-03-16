f5b5f8c016d6c046f6e999393a2c4b2fe9dd5a06
JR <jesse.ronkko@eng.tamk.fi>
2019-03-16 18:38:10 +0200

    getNews with appId test succeeded

--- src/App.js
+++ src/App.js
@@ -5,32 +5,50 @@ const API_KEY = process.env.REACT_APP_STEAM_API_KEY;
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {}
-    };
+  state = {
+    appId: 'original',
   }
 
-  componentDidMount() {
-    var url = `https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${API_KEY}&steamid=76561197980934566&format=json`;
-    console.log(url);
-    fetch(url).then(
-      response => response.json()
-    ).then(
-      json => {
-        console.log(json);
-        this.setState({data: json});
+  getNews = (e) => {
+    
+    //prevents page refresh
+    e.preventDefault();
+
+    //completing the URL
+    var homeURL = "http://localhost:4000/getnews/?"
+    var userInput = this.state.appId;
+    var newURL = `${homeURL}${userInput}`;
+
+    var req = new XMLHttpRequest();
+    req.open("GET", newURL, true);
+    req.addEventListener('load', function(){
+      if(req.status>= 200 && req.status<400){
+      var response = JSON.parse(req.responseText);
+      console.log(JSON.parse(req.responseText));
       }
-    );
-  }
+      else {
+        console.log("Error in network request: " + response.statusText);
+      }
+    });
+    req.send(null);
+}
 
+handleChange = (e) => {
+  this.setState ({
+    appId: e.target.value
+  });
+}
 
   render() {
     return (
       <div className="App">
         <h1>Steam Game Time Viewer</h1>
         <p>Welcome!</p>
+        <p>appId: {this.state.appId}</p>
+        <form onSubmit={this.getNews}>
+          <input type="text" onChange={this.handleChange}/>
+          <button>GET NEWS</button>
+        </form>
       </div>
     );
   }
